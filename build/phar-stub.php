882b3835260d3e6dc65ce79a3f45c96a27cce3fc
Mike Tougeron <mtougeron@ign.com>
2013-08-08 08:22:36 -0700

    Add the copyright information to the header of each file.

--- build/phar-stub.php
+++ build/phar-stub.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * @package    klout-sdk-php
+ * @author     Mike Tougeron <github+klout-sdk-php@tougeron.com>
+ * @copyright  Copyright (c) 2013 Mike Tougeron <github+klout-sdk-php@tougeron.com>
+ * @license    http://opensource.org/licenses/MIT
+ * @link       https://github.com/mtougeron/klout-sdk-php
+ */
 
 Phar::mapPhar('klout.phar');
 
--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * @package    klout-sdk-php
+ * @author     Mike Tougeron <github+klout-sdk-php@tougeron.com>
+ * @copyright  Copyright (c) 2013 Mike Tougeron <github+klout-sdk-php@tougeron.com>
+ * @license    http://opensource.org/licenses/MIT
+ * @link       https://github.com/mtougeron/klout-sdk-php
+ */
 
 if (!$loader = include __DIR__.'/../vendor/autoload.php') {
     die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
