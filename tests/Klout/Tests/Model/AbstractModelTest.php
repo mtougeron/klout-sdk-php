213ada0a10cfaf6c5b0ea0c12422db74a2124848
Mike Tougeron <mtougeron@mashery.com>
2013-12-20 12:32:20 -0800

    Add unittests for Klout\Model\AbstractModel

--- tests/Klout/Tests/Model/AbstractModelTest.php
+++ tests/Klout/Tests/Model/AbstractModelTest.php
@@ -9,17 +9,53 @@
 
 namespace Klout\Tests\Model;
 
+use Klout\Tests\Model\AbstractModelStub;
+
 class AbstractModelTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function setUp()
+    /**
+     * Tests that an empty object (no protected/private properties)
+     * will still return an array but is empty
+     *
+     * @covers \Klout\Model\AbstractModel::toArray
+     */
+    public function testToArrayEmptyObject()
     {
-        //615089
+        $class = $this->getMockForAbstractClass('Klout\Model\AbstractModel');
+        $results = $class->toArray();
+        $this->assertInternalType('array', $results);
+        $this->assertCount(0, $results);
     }
 
-    public function testFakeTest()
+    /**
+     * Tests that only the protected & private properties of the AbstractModelStub
+     * returns data in the array
+     *
+     * @covers \Klout\Model\AbstractModel::toArray
+     */
+    public function testToArray()
     {
-        $this->assertGreaterThan(0, 1);
+        $class = new AbstractModelStub();
+        // Set an instance of AbstractModelStub so that the recursion happens
+        $class->setMyProtectedObjectVarWithToArrayMethod(new AbstractModelStub());
+        $results = $class->toArray();
+
+        $this->assertInternalType('array', $results);
+        $this->assertCount(3, $results);
+        $this->assertArrayHasKey('myProtectedVarWithMethod', $results);
+        $this->assertArrayHasKey('myPrivateVarWithMethod', $results);
+        $this->assertArrayHasKey('myProtectedObjectVarWithToArrayMethod', $results);
+
+        $this->assertInternalType('array', $results['myProtectedObjectVarWithToArrayMethod']);
+        $this->assertCount(3, $results['myProtectedObjectVarWithToArrayMethod']);
+        $this->assertArrayHasKey('myProtectedVarWithMethod', $results['myProtectedObjectVarWithToArrayMethod']);
+        $this->assertArrayHasKey('myPrivateVarWithMethod', $results['myProtectedObjectVarWithToArrayMethod']);
+        $this->assertArrayHasKey('myProtectedObjectVarWithToArrayMethod', $results['myProtectedObjectVarWithToArrayMethod']);
+
+        // The recursion should set this property but it doesn't have a value set
+        $this->assertNull($results['myProtectedObjectVarWithToArrayMethod']['myProtectedObjectVarWithToArrayMethod']);
+
     }
 
 }
